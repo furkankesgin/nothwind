eda6971303ab7a083283989305b352e474949a95
Furkan Kesgin <furkan_bfk@hotmail.com>
2022-07-30 10:34:13 +0300

    first commit

---
+++ src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,10 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result{
+
+	public DataResult(T data, boolean success, String message) {
+		super(success, message);
+		
+	}
+
+}
