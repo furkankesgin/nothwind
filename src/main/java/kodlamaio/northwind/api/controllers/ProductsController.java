8f2898f628d7cd26592de960416fee82f3b21eb5
Furkan Kesgin <furkan_bfk@hotmail.com>
2022-08-09 16:19:16 +0300

    spring part finished

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController
 @RequestMapping("/api/products")
@@ -27,7 +33,43 @@ public class ProductsController {
 
 
 	@GetMapping("/getall")
-	public List<Product> getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
 	}
+	
+	@GetMapping("/getProductWithCategoryDetails") // veri istedeğimiz zaman bunlar get request deniliyor bu yüzden getmapping // yapıyoruz.
+    public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails() {
+        return this.productService.getProductWithCategoryDetails();
+    }
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product){
+		return this.productService.add(product);
+	}
+	
+	@GetMapping("/getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName){
+		return this.productService.getByProductName(productName);
+	}
+	
+	@GetMapping("/getByProductNameAndCategoryId")
+    public DataResult<Product> getByProductNameAndCategory(@RequestParam("productName") String productName, @RequestParam("categoryId") int categoryId){
+        return this.productService.getByProductNameAndCategoryId(productName, categoryId);
+    }
+    
+    @GetMapping("/getByProductNameContains")
+    public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){    // arama kısmı burası 
+        return this.productService.getByProductNameContains(productName);    
+    }
+    
+    @GetMapping("/getAllByPage")
+    DataResult<List<Product>>  getAll(int pageNo , int pageSize){
+        return this.productService.getAll(pageNo -1,pageSize);
+    }
+    
+    @GetMapping("/getAllDesc")
+    public DataResult<List<Product>> getAllSorted() {
+    	return this.productService.getAllSorted();
+    }
+    
 }
--- src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,41 @@
  package kodlamaio.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductDao extends JpaRepository<Product, Integer>{ //basta hangi entitye bakicaksin ve primary keyin tipini iyaziyoruz
+	// tabloya bakıyor getBy a göre where koşulu oluştuuryor kendisi jparepo yapıyor 
 
+    Product getByProductName(String productName);   // select * from products where product_name = emre
+    
+                //1. kolon       2. kolon
+    Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);  // select * from products where product_name = emre  and category_id = 1
+
+                //1. kolon       2. kolon
+    List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId); // select * from products where category_id in(1,2,3,4)
+                // isimlendirme kuralına uyman yeterli
+    
+    List<Product> getByCategoryIn(List<Integer>categories); 
+    
+    List<Product> getByProductNameContains(String productName);
+    
+    List<Product> getByProductNameStartsWith(String productName);
+    
+        // veritabaını query yazarken unut
+        //  hangi entityden  hangi alan     parametrem 
+    @Query("From Product where productName=:productName and category.categoryId=:categoryId")  // select * from products where product_name = bisey and category_id = bisey  
+    List<Product> getByNameAndCategory(String productName, int categoryId);
+    
+//    @Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto(p.id, p.productName, c.categoryName) From Category c Inner Join c.products p")         //select p.Product_id, p.product_name, c.category_name from Category c inner join Product p on c.categoryId = p.categoryId        
+    // ana tablodan başlayın ilk  2 tabloyu bi tablo yapmamızı sağlıyor
+//    List<ProductWithCategoryDto> getProductWithCategoryDetails(); // imza bu
+    
+    
+    @Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto(p.id, p.productName, c.categoryName) From Category c Inner Join c.products p")
+    List<ProductWithCategoryDto> getProductWithCategoryDetails();
 }
